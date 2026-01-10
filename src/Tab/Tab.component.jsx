5ae07fa8ffc124186445535355f92eb2ff276f25
crotch-watch <naps_08_bay@icloud.com>
2026-01-10 23:56:53 +0530

    removed js

--- src/Tab/Tab.component.jsx
+++
@@ -1,8 +0,0 @@
-export const Tab = ({ id, name, onClose, handleTabClick }) => {
-  return (
-    <header key={id} onClick={handleTabClick}>
-      {name}
-      <span onClick={onClose}>x</span>
-    </header>
-  )
-}
--- src/Tab/Tab.constants.js
+++
@@ -1,8 +0,0 @@
-export const DEFAULT_TAB_STORE = []
-
-export const TAB_STORE_OPS = {
-  create: "create",
-  delete: "delete"
-}
-
-Object.freeze(TAB_STORE_OPS)
