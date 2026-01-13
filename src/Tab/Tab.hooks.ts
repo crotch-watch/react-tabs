20c30e450c9686695709ed82bdf49d4383e06ea6
crotch-watch <naps_08_bay@icloud.com>
2026-01-13 23:17:54 +0530

    abstracted side-effects on different FSM modes and cleanup

--- src/Tab/Tab.hooks.ts
+++ src/Tab/Tab.hooks.ts
@@ -1,14 +1,7 @@
-import { useReducer } from "react"
-import { useState } from "react"
+import { useEffect, useReducer, useRef } from "react"
 import { tabStoreReducer } from "./Tab.Reducers"
 import { DEFAULT_TAB_STATE } from "./Tab.constants.ts"
-import type { Tab } from "./Tab.types.ts"
-
-export const useActiveTab = (id = Infinity) => {
-  const [activeTabId, setActiveTabId] = useState(id)
-
-  return { activeTabId, setActiveTabId }
-}
+import type { Mode, Tab } from "./Tab.types.ts"
 
 export const useTabs = () => {
   const [state, dispatch] = useReducer(tabStoreReducer, DEFAULT_TAB_STATE)
@@ -29,54 +22,48 @@ export const useTabs = () => {
     dispatch({ type: "DELETE_TAB", payload: uid })
   }
 
+  const drag = (uid: Tab["uid"]) => {
+    dispatch({ type: "DRAGGING_ELEMENT", payload: uid })
+  }
+
+  const end = (uid: Tab["uid"]) => {
+    dispatch({ type: "ENDING_DRAG", payload: uid })
+  }
+
   return {
     state,
     requestDataEntry,
     enterData,
     changeActiveTab,
     deleteTab,
+    drag,
+    end,
   }
 }
 
-// export const useTabStore = () => {
-//   const [tabStore, dispatch] = useReducer(tabStoreReducer, DEFAULT_TAB_STORE)
+export const useDragDropEffects = (elementId: string, mode: Mode) => {
+  const child = useRef<HTMLElement | null>(null)
 
-//   const remove = (id: Tab["uid"]) => {
-//     dispatch({ type: TAB_STORE_OPS.delete, payload: id })
-//   }
+  useEffect(() => {
+    switch (mode) {
+      case "dragging": {
+        child.current = document.getElementById(elementId)
 
-//   const create = (tab: Tab) => {
-//     dispatch({ type: TAB_STORE_OPS.create, payload: tab })
-//   }
+        break
+      }
 
-//   return { tabStore, remove, create }
-// }
+      case "idle": {
+        if (child.current === null) return
+        if (child.current.parentElement === null) return
 
-// export const useDragDrop = () => {
-//   const { tabStore, remove, create } = useTabStore()
-//   const { activeTabId, setActiveTabId } = useActiveTab()
+        child.current.parentElement.appendChild(child.current)
 
-//   const [mode, setMode] = useState("idle")
+        break
+      }
+    }
 
-//   useEffect(() => {
-//     switch (mode) {
-//       case "idle":
-//         return null
-
-//       // state has changed to dragging mode
-//       // idle -> dragging
-//       case "dragging": {
-//         // reset to default if all tabs are empty
-//         const onlyElement = activeTabId === 0
-//         if (onlyElement) return setActiveTabId(Infinity)
-
-//         // if deleted tab is first tab set active tab to next.
-//         const firstTab = activeTabId === 1
-//         if (firstTab) setActiveTabId((prevActiveTab) => prevActiveTab + 1)
-//         else setActiveTabId((prevActiveTab) => prevActiveTab - 1)
-//       }
-//     }
-//   }, [mode])
-
-//   return {}
-// }
+    return () => {
+      child.current = null
+    }
+  }, [mode])
+}
