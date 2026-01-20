1b6d779ac74b089ef6d4a705d59b77a4e4ce8bdf
crotch-watch <naps_08_bay@icloud.com>
2026-01-20 16:43:50 +0530

    added onSubmit controller for tab re-arrangement

--- src/Tab/TabGroup.component.tsx
+++ src/Tab/TabGroup.component.tsx
@@ -1,13 +1,13 @@
 import { useRef } from "react"
 import { TabC } from "./Tab.component"
-import { useDragDropEffects, useTabs } from "./Tab.hooks"
+import { useTabs } from "./Tab.hooks"
 
 export const TabGroup = () => {
   const {
     state: {
       data,
-      view: { activeTabId, draggingTabId },
-      mode,
+      view: { activeTabId },
+      mode
     },
     enterData,
     requestDataEntry,
@@ -15,9 +15,8 @@ export const TabGroup = () => {
     deleteTab,
     drag,
     end,
+    drop
   } = useTabs()
-
-  useDragDropEffects(draggingTabId, mode)
 
   const nameRef = useRef<HTMLInputElement | null>(null)
   const dataRef = useRef<HTMLInputElement | null>(null)
@@ -31,7 +30,7 @@ export const TabGroup = () => {
     enterData({
       uid: Math.random().toString(),
       name: nameRef.current.value.trim(),
-      data: dataRef.current.value.trim(),
+      data: dataRef.current.value.trim()
     })
 
     nameRef.current.value = dataRef.current.value = ""
@@ -46,69 +45,114 @@ export const TabGroup = () => {
           id={tab.uid}
           draggable={true}
           onDragStart={() => drag(tab.uid)}
-          onDragEnd={({ clientX, clientY }) => {
+          onDragEnd={(event) => {
+            const { clientX, clientY } = event
+
             const droppable = document.getElementById("droppable")
 
             if (!droppable) return
-            const dropped = document.elementFromPoint(clientX, clientY)
+            const elementAtDropCoords = document.elementFromPoint(
+              clientX,
+              clientY
+            )
 
-            if (!dropped) return
-
-            if (droppable.contains(dropped)) {
-              // equivalent to on onDrop
-
-              function calc2d({
-                x1,
-                x2,
-                y1,
-                y2,
-              }: {
-                x1: number
-                x2: number
-                y1: number
-                y2: number
-              }) {
-                const greaterX = x2 - x1 > 0 ? x2 : x1
-                const greaterY = y2 - y1 > 0 ? y2 : y1
-
-                return Math.pow(
-                  Math.pow(greaterX - (greaterX === x2 ? x1 : x2), 2) +
-                    Math.pow(greaterY - (greaterY === y2 ? y1 : y2), 2),
-                  0.5
-                )
-              }
-
-              function calcBoxCenter(box: DOMRect) {
-                return {
-                  x: box.width / 2 + box.left,
-                  y: box.height / 2 + box.top,
-                }
-              }
-
-              let closetDroppable: Element
-              let min: number = Infinity
-
-              const droppables = [...droppable.children]
-              droppables.forEach((droppable) => {
-                const droppableCoords = droppable.getBoundingClientRect()
-                const droppableCenter = calcBoxCenter(droppableCoords)
-                const distance = calc2d({
-                  x2: clientX,
-                  x1: droppableCenter.x,
-                  y2: clientY,
-                  y1: droppableCenter.y,
-                })
-
-                if (distance > min) return
-
-                min = distance
-                closetDroppable = droppable
-              })
-
+            const droppedInDroppable = droppable.contains(elementAtDropCoords)
+            if (!elementAtDropCoords || !droppedInDroppable) {
+              end(tab.uid)
               return
             }
 
-            end(tab.uid)
+            // tab is dropped in droppable
+            function calc2d({
+              x1,
+              x2,
+              y1,
+              y2
+            }: {
+              x1: number
+              x2: number
+              y1: number
+              y2: number
+            }) {
+              const greaterX = x2 - x1 > 0 ? x2 : x1
+              const greaterY = y2 - y1 > 0 ? y2 : y1
+
+              return Math.sqrt(
+                Math.pow(greaterX - (greaterX === x2 ? x1 : x2), 2) +
+                  Math.pow(greaterY - (greaterY === y2 ? y1 : y2), 2)
+              )
+            }
+
+            function calcBoxCenter(box: DOMRect) {
+              return {
+                x: box.width / 2 + box.left,
+                y: box.height / 2 + box.top
+              }
+            }
+
+            let closetDraggable: Element
+            let min: number = Infinity
+            let droppedTowards: "left" | "right" = "left"
+
+            const droppables = [...droppable.children]
+            droppables.forEach((droppable) => {
+              const droppableCoords = droppable.getBoundingClientRect()
+              const droppableCenter = calcBoxCenter(droppableCoords)
+
+              const distance = calc2d({
+                x2: clientX,
+                x1: droppableCenter.x,
+                y2: clientY,
+                y1: droppableCenter.y
+              })
+
+              if (distance > min) return
+
+              min = distance
+              closetDraggable = droppable
+            })
+
+            if (
+              clientX >
+              calcBoxCenter(closetDraggable!?.getBoundingClientRect()).x
+            ) {
+              droppedTowards = "right"
+            }
+
+            const closestElementIndex = data.findIndex(
+              (tab) => tab.uid === closetDraggable.id
+            )
+
+            const target = event.target as Element
+
+            const droppedAtSameIndex = +target.id === +closetDraggable!.id
+            if (droppedAtSameIndex) {
+              end(elementAtDropCoords.id)
+              return
+            }
+
+            const isLeftMostDropoff =
+              closestElementIndex === 0 && droppedTowards === "left"
+
+            if (isLeftMostDropoff) {
+              drop({ index: 0, uid: tab.uid })
+              return
+            }
+
+            const isRightMostDropoff =
+              closestElementIndex === data.length - 1 &&
+              droppedTowards === "right"
+
+            if (isRightMostDropoff) {
+              drop({ index: data.length - 1, uid: tab.uid })
+            }
+
+            const dropoffIndex =
+              droppedTowards === "left"
+                ? closestElementIndex - 1
+                : closestElementIndex + 1
+
+            drop({ index: dropoffIndex, uid: target.id })
           }}
         >
           <TabC
@@ -116,8 +160,6 @@ export const TabGroup = () => {
             onClose={() => {
               deleteTab(tab.uid)
             }}
-            onDragStart={() => drag(tab.uid)}
-            onDragEnd={() => end(tab.uid)}
             handleTabClick={() => changeActiveTab(tab.uid)}
           />
         </div>
@@ -139,18 +181,12 @@ export const TabGroup = () => {
           <form onSubmit={handleFormSubmit}>
             <label htmlFor="input--create-tab">
               name
-              <input
-                ref={nameRef}
-                id="input--create-tab"
-              />
+              <input ref={nameRef} id="input--create-tab" />
             </label>
 
             <label htmlFor="textarea--add-tab-desc">
               data
-              <input
-                ref={dataRef}
-                id="textarea--add-tab-desc"
-              />
+              <input ref={dataRef} id="textarea--add-tab-desc" />
             </label>
 
             <button>add</button>
